480f69b9a7c1fd7d87427c6606a85253240fbcc6
Sasha-Gor32 <sasha051505@gmail.com>
2024-11-28 16:50:30 +0300

    Update module_10_5.py

--- module_10_5.py
+++ module_10_5.py
@@ -1,26 +1,24 @@
-import datetime
 import multiprocessing
-
-
-
-
+from datetime import datetime
 def read_info(name):
     all_data = []
-    with open(name, 'r') as f:
-        line = f.readline()
-        if not line:
-            break
-        else:
+    with open(name, 'r', encoding='utf8') as file1:
+        while True:
+            line = file1.readline().strip()
             all_data.append(line)
-filenames = [f'./file_{number}.txt' for number in range(1, 5)]
-start = datetime.datetime.now()
-for name in filenames:
-    read_info(name)
-end = datetime.datetime.now()
-print(end - start)
+            if not line:
+                break
+files = ['file 1.txt', 'file 2.txt', 'file 3.txt', 'file 4.txt']
+start1 = datetime.now()
+for i in files:
+    read_info(i)
+end1 = datetime.now()
+time_of_line_function = end1 - start1
+print(f'Время работы линейного вызова : {time_of_line_function}')
 if __name__ == '__main__':
-    start = datetime.datetime.now()
-    with multiprocessing.Pool(processes=8) as pool:
-        pool.map(read_info, filenames)
-    end = datetime.datetime.now()
-    print(end - start)
+    start2 = datetime.now()
+    with multiprocessing.Pool(processes=4) as pool:
+        pool.map(read_info, files)
+    end2 = datetime.now()
+    time_of_multiprocessing = end2 - start2
+    print(f'Время работы мультипроцесса : {time_of_multiprocessing}')
